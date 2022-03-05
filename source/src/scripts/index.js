84c4d0d31bde79d0a124c857a96066991d4536a6
exslym <76844562+exslym@users.noreply.github.com>
2022-03-05 10:22:06 +0300

    project ready

---
+++ source/src/scripts/index.js
@@ -0,0 +1,10 @@
+import '../styles/index.scss';
+// import * as jQuery from '../../public/tools/jquery-3.6.0.min.js';
+
+if (process.env.NODE_ENV === 'development') {
+	require('../index.html');
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+	'use strict';
+});
